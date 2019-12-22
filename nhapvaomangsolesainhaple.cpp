6d221a1775f1d739bc5aeee0597dfcd5977a0949
minhphqhn <minhphqth1910008@fpt.edu.vn>
2019-12-22 16:13:37 +0700

    labs6

---
+++ nhapvaomangsolesainhaple.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int ary[10];
+	for (int i = 0; i < 10; ++i)
+	{
+		do{
+			printf("Nhap ary[%d]=\n",i);
+			scanf("%d",&ary[i]);
+		}while(ary[i] %2==0);
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		printf("ary[%d]= %d\n",i,ary[i]);
+	}
+	return 0;
+}
---
+++ timsoletrongmang.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("Nhap n=\n");
+	scanf("%d",&n);
+	int ary[n];
+	for(int i=0;i<n;i++){
+		printf("Nhap ary[%d]=\n",i);
+		scanf("%d",&ary[i]);
+	}
+	for(int i=0;i<n;i++){
+		if(ary[i]%2!=0){
+			printf("So le =%d\n",ary[i]);
+		}
+	}
+}
